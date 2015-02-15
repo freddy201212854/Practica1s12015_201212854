0a29479138faf918ce90593964b5b162986d5a43
KRATOS <KRATOS@KRATOS-PC>
2015-02-15 01:08:54 -0600

    new commit

---
+++ src/practica1_edd/Personajes.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1_edd;
+
+/**
+ *
+ * @author KRATOS
+ */
+public class Personajes {
+    
+    String Nombre;
+    int PtsAtaque;
+    int PtsDefensa;
+    String TipoDisparo;
+    
+    public Personajes(String Nombre,int PtsAtaque,int PtsDefensa,String TipoDisparo)
+    {
+        this.Nombre=Nombre;
+        this.PtsAtaque=PtsAtaque;
+        this.PtsDefensa=PtsDefensa;
+        this.TipoDisparo=TipoDisparo;
+    }
+    
+    public String getNombre()
+    {
+        return this.Nombre;
+    }
+    public void setNombre(String nombre)
+    {
+        this.Nombre=nombre;
+    }
+    public int getAtaque()
+    {
+        return this.PtsAtaque;
+    }
+    public void setAtaque(int ataque)
+    {
+        this.PtsAtaque=ataque;
+    }
+    public int getDefensa()
+    {
+        return this.PtsDefensa;
+    }
+    public void setDefensa(int defensa)
+    {
+        this.PtsDefensa=defensa;
+    }
+}
